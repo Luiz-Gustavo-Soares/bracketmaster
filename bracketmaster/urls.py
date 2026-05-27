e3f9a509733e2e627549acd7c8065c89923db3e0
Luiz Gustavo <gustavo120wa@gmail.com>
2026-05-27 18:58:53 -0300

    base html

--- bracketmaster/urls.py
+++ bracketmaster/urls.py
@@ -20,9 +20,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
     path('users/', include('users.urls')),
 ]
 
---
+++ bracketmaster/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, get_object_or_404, redirect
+
+
+
+def home(request):
+    """Pagina inicial da aplicação"""
+    user = request.user
+    return render(request, 'bracketmaster/home.html')
