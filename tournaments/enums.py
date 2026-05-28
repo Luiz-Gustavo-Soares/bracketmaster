da52cb98b1637cdab1fe1818725b04e3d5159bf2
Luiz Gustavo <gustavo120wa@gmail.com>
2026-05-27 23:08:33 -0300

    inicio da modelagem dos torneios

--- tournaments/enums.py
+++ tournaments/enums.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class TipoTorneio(models.TextChoices):
     CASUAL = "CA", "Casual"
-    COMPETITIVA = "CO", "Competitiva"
+    COMPETITIVO = "CO", "Competitivo"
 
 class FormatoJogo(models.TextChoices):
     STANDARD = "ST", "Standard"
@@ -11,3 +11,22 @@ class FormatoJogo(models.TextChoices):
     MODERN = "MO", "Modern"
     PIONEER = "PI", "Pioneer"
     DRAFT = "DR", "Draft"
+
+
+class StatusTorneio(models.TextChoices):
+    CRIADO = 'CRIADO'
+    INSCRICOES = 'INSCRICOES'
+    INSCRICOES_E = 'INSCRICOES_ENCERRADAS'
+    EM_ANDAMENTO = 'EM_ANDAMENTO'
+    FINALIZADO = 'FINALIZADO'
+
+
+class FormatoTorneio(models.TextChoices):
+    SWISS = 'SWISS', 'Sistema Suiço'
+    SINGLE_ELIM = 'SINGLE_ELIM', 'Eliminação Simples'
+
+
+class StatusRodada(models.TextChoices):
+    ABERTA = 'AB', 'Aberta'
+    FINALIZADA = 'FI', 'Finalizada'
+    
--- tournaments/models.py
+++ tournaments/models.py
@@ -1,3 +1,99 @@
 from django.db import models
+from .enums import FormatoJogo, TipoTorneio, FormatoTorneio, StatusTorneio, StatusRodada
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Torneio(models.Model):
+    nome = models.CharField(max_length=150)
+    descricao = models.TextField(blank=True, null=True)
+
+    organizador = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        related_name='torneios_criados'
+    )
+
+    tipo = models.CharField(
+        max_length=2,
+        choices=TipoTorneio.choices,
+        default=TipoTorneio.COMPETITIVO
+    )
+
+    formato_torneio = models.CharField(
+        max_length=20,
+        choices=FormatoTorneio.choices
+    )
+
+    status = models.CharField(
+        max_length=20,
+        choices=StatusTorneio.choices,
+        default=StatusTorneio.CRIADO
+    )
+
+    formato_jogo = models.CharField(
+        max_length=2,
+        choices=FormatoJogo.choices,
+        default=FormatoJogo.COMMANDER
+    )
+
+    numero_rodadas = models.PositiveIntegerField()
+
+    data_inicio = models.DateTimeField()
+
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+
+
+class TorneioParticipante(models.Model):
+
+    torneio = models.ForeignKey(
+        Torneio,
+        on_delete=models.CASCADE,
+        related_name='participantes'
+    )
+
+    jogador = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+
+    pontos = models.IntegerField(default=0)
+
+    vitorias = models.IntegerField(default=0)
+
+    derrotas = models.IntegerField(default=0)
+
+    empates = models.IntegerField(default=0)
+
+    class Meta:
+        unique_together = [
+            ('torneio', 'jogador')
+        ]
+
+class Rodada(models.Model):
+
+    torneio = models.ForeignKey(
+        Torneio,
+        on_delete=models.CASCADE,
+        related_name='rodadas'
+    )
+
+    numero = models.PositiveIntegerField()
+
+    status = models.CharField(
+        max_length=20,
+        choices=StatusRodada.choices,
+        default=StatusRodada.ABERTA
+    )
+
+    criada_em = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    class Meta:
+        unique_together = [
+            ('torneio', 'numero')
+        ]
+
+        ordering = ['numero']
