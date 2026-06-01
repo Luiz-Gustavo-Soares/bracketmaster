e2b05edf0951f944a5f817dc940215c5ecaebdf6
Luiz Gustavo <gustavo120wa@gmail.com>
2026-06-01 17:04:13 -0300

    imagem auth / caminho index view / media gitignore

--- bracketmaster/views.py
+++ bracketmaster/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render, get_object_or_404, redirect
 def home(request):
     """Pagina inicial da aplicação"""
     user = request.user
-    return render(request, 'bracketmaster/home.html')
+    return render(request, 'index.html')
