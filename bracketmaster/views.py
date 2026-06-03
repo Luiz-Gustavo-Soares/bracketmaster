1ac2eb2c6db89e2c8e3bdfadd37611987f853c51
Italo Felipe <121637958+Iokanaan-dev@users.noreply.github.com>
2026-06-03 16:25:32 -0300

    Criação de status de campeonato
    Criação dos status no arquivo status_badge
    
    Criação do design em componentes com .badge

--- bracketmaster/views.py
+++ bracketmaster/views.py
@@ -8,13 +8,24 @@ def home(request):
     # Simulando o que virá do Banco de Dados futuramente pra testar essa bomba
     torneios_mock = [
         {
+            'titulo': 'A Última Evolução',
+            'descricao': 'A perfeição está próxima. Em um mundo à beira do colapso, apenas aqueles capazes de evoluir além dos limites sobreviverão. A última etapa da transformação começou.',
+            'formato': 'Commander',
+            'data': '29/06/26',
+            'imagem_capa': 'media/capas_campeonato/monumento_magic.jpg',
+            'autor_nome': 'Atraxa',
+            'autor_avatar': 'media/avatares/Atraxa.jpg',
+            'status': 'created' 
+        },
+        {
             'titulo': 'Mix Diamantina Dia D de Dado',
             'descricao': 'Boas-vindas ao melhor campeonato de Commander do mundo, e até de Diamantina!',
             'formato': 'Commander',
             'data': '14/06/26',
             'imagem_capa': 'media/capas_campeonato/lobinho_magic.png',
             'autor_nome': 'Nissa Revane',
-            'autor_avatar': 'media/avatares/nissa.png'
+            'autor_avatar': 'media/avatares/nissa.png',
+            'status': 'open' 
         },
         {
             'titulo': 'Maior Campeonato de Todos os Tempos',
@@ -23,7 +34,8 @@ def home(request):
             'data': '21/06/26',
             'imagem_capa': 'media/capas_campeonato/cidade_vermelha.png',
             'autor_nome': 'Gideon Jura',
-            'autor_avatar': 'media/avatares/gideon.png'
+            'autor_avatar': 'media/avatares/gideon.png',
+            'status': 'running' 
         },
         {
             'titulo': 'Só Dinos e Mais Nada, Uhum!',
@@ -32,7 +44,8 @@ def home(request):
             'data': '01/07/26',
             'imagem_capa': 'media/capas_campeonato/dinos_magic.png',
             'autor_nome': 'Gishath, Avatar do Sol',
-            'autor_avatar': 'media/avatares/gisha.png'
+            'autor_avatar': 'media/avatares/gisha.png',
+            'status': 'closed' 
         },
         {
             'titulo': 'Redraft Gótico de Innistrad Muito Goth',
@@ -41,7 +54,8 @@ def home(request):
             'data': '14/08/26',
             'imagem_capa': 'media/capas_campeonato/pretos.png',
             'autor_nome': 'Liliana Vess',
-            'autor_avatar': 'media/avatares/lilia.png'
+            'autor_avatar': 'media/avatares/lilia.png',
+            'status': 'finished' 
         }
     ]
 
