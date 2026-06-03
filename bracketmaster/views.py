e9832ad257168f7ed848263f557ae70dd05d0488
Italo Felipe <121637958+Iokanaan-dev@users.noreply.github.com>
2026-06-03 13:36:59 -0300

    Refinamento do layout de Auth e aprimora UI da Landing Page
    Correção do bug de rolagem nas telas de Login/Cadastro usando 100dvh e posicionamento absoluto no header/footer.
    
    Ajuste no enquadramento (center) do background das telas de autenticação.
    
    Aumento do Hero Button (CTA principal) para dar mais destaque.
    
    Refatoração da seção de Benefícios para o formato de Cards interativos.
    
    Substituição de caracteres de texto por ícones SVG reais na seção de Benefícios.
    
    Reorganização do Footer principal e adição do disclaimer legal da Wizards of the Coast.
    
    Adicionamento de icons com biblioteca importata no base.html

--- bracketmaster/views.py
+++ bracketmaster/views.py
@@ -12,36 +12,36 @@ def home(request):
             'descricao': 'Boas-vindas ao melhor campeonato de Commander do mundo, e até de Diamantina!',
             'formato': 'Commander',
             'data': '14/06/26',
-            'imagem_capa': 'media/bg-auth.jpg',
+            'imagem_capa': 'media/capas_campeonato/lobinho_magic.png',
             'autor_nome': 'Nissa Revane',
-            'autor_avatar': 'media/bg-auth.jpg'
+            'autor_avatar': 'media/avatares/nissa.png'
         },
         {
             'titulo': 'Maior Campeonato de Todos os Tempos',
             'descricao': 'Aqui decidiremos o próximo CEO da Wizards of the Coast, porque o trem tá feio.',
             'formato': 'Modern',
             'data': '21/06/26',
-            'imagem_capa': 'media/bg-auth.jpg',
+            'imagem_capa': 'media/capas_campeonato/cidade_vermelha.png',
             'autor_nome': 'Gideon Jura',
-            'autor_avatar': 'media/bg-auth.jpg'
+            'autor_avatar': 'media/avatares/gideon.png'
         },
         {
             'titulo': 'Só Dinos e Mais Nada, Uhum!',
             'descricao': 'Tá no nome. Se vier encher o saco te lapo a mordida, ou o meu amigo Joãozinho comedor de ovo!',
             'formato': 'Commander',
             'data': '01/07/26',
-            'imagem_capa': 'media/bg-auth.jpg',
+            'imagem_capa': 'media/capas_campeonato/dinos_magic.png',
             'autor_nome': 'Gishath, Avatar do Sol',
-            'autor_avatar': 'media/bg-auth.jpg'
+            'autor_avatar': 'media/avatares/gisha.png'
         },
         {
             'titulo': 'Redraft Gótico de Innistrad Muito Goth',
             'descricao': 'Venha de preto.',
             'formato': 'Draft',
             'data': '14/08/26',
-            'imagem_capa': 'media/bg-auth.jpg',
+            'imagem_capa': 'media/capas_campeonato/pretos.png',
             'autor_nome': 'Liliana Vess',
-            'autor_avatar': 'media/bg-auth.jpg'
+            'autor_avatar': 'media/avatares/lilia.png'
         }
     ]
 
