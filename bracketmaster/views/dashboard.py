4743036d7480737003f709b94b7da29e3377c8bd
Italo Felipe <121637958+Iokanaan-dev@users.noreply.github.com>
2026-06-03 20:26:55 -0300

    Normalização da navbar, e aprimoramento da IU no dashboard
    Criei um novo arquivo css especifico para o dashboard.
    
    A partir de agora está pronto para a implementação das telas internas.

--- bracketmaster/views/dashboard.py
+++ bracketmaster/views/dashboard.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    return render(request, 'dashboard/dashboard.html')
