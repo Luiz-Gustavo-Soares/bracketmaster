6cbd0bc9bd578f83fbe7cdfee16078fff372d754
Italo Felipe <121637958+Iokanaan-dev@users.noreply.github.com>
2026-06-04 16:51:22 -0300

    Tirando do main.js oq não é global

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,4 @@
 /*helpers globais, alerts auto-dismiss*/
-window.addEventListener('scroll', function() { //VAi ficar esperando um evento, no caso mexer no scroll se acontecer chama a função
-    const scrollIndicator = document.getElementById('scrollIndicator'); // pega o id na nossa linha
-    
-    if (scrollIndicator) { 
-        const winScroll = document.body.scrollTop || document.documentElement.scrollTop; // quantos pixels rodou 
-        const height = document.documentElement.scrollHeight - document.documentElement.clientHeight; //maximo de pixels antes de bater no rodape da tela
-        const scrolled = (winScroll / height) * 100; //descobrir a porcentagem usando a posição atual e o restante total
-        scrollIndicator.style.width = scrolled + "%"; // pega a % e taca na linha, especificamente na largura, assim ela cresce igual a rosca do calabreso
-    }
-});
 
 document.addEventListener('DOMContentLoaded', function() {
     
