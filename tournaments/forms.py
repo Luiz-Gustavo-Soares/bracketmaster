057b7459cd1c9eb60ec72b908b84b5aa636cc36c
Luiz Gustavo <gustavo120wa@gmail.com>
2026-06-08 22:56:33 -0300

    feat: formulario de torneio

---
+++ tournaments/forms.py
@@ -0,0 +1,109 @@
+from django import forms
+from tournaments.models import Torneio
+
+class TorneioForm(forms.ModelForm):
+
+    cidade = forms.CharField(
+        widget = forms.TextInput(
+            attrs={
+
+            }
+        )
+    )
+
+    estado = forms.CharField(
+        widget = forms.TextInput(
+            attrs={
+                
+            }
+        )
+    )
+
+    class Meta:
+
+        model = Torneio
+
+        fields = [
+            'nome',
+            'descricao',
+            'numero_maximo_participantes',
+            'valor_inscricao',
+            'local',
+            'premiacoes',
+            'tipo',
+            'formato_torneio',
+            'status',
+            'formato_jogo',
+            'numero_rodadas',
+            'data_inicio'
+        ]
+
+        widgets = {
+
+            'nome': forms.TextInput(
+                attrs={
+
+                }
+            ),
+
+            'descricao': forms.Textarea(
+                attrs={
+                }
+            ),
+
+            'numero_maximo_participantes': forms.NumberInput(
+                attrs={
+                }
+            ),
+
+            'valor_inscricao': forms.NumberInput(
+                attrs={
+
+                }
+            ),
+
+            'cidade': forms.TextInput(
+                attrs={
+                }
+            ),
+
+            'local': forms.TextInput(
+                attrs={
+                }
+            ),
+
+            'premiacoes': forms.Textarea(
+                attrs={
+                }
+            ),
+
+            'tipo': forms.Select(
+                attrs={
+                }
+            ),
+
+            'formato_torneio': forms.Select(
+                attrs={
+                }
+            ),
+
+            'status': forms.Select(
+                attrs={
+                }
+            ),
+
+            'formato_jogo': forms.Select(
+                attrs={
+                }
+            ),
+
+            'numero_rodadas': forms.NumberInput(
+                attrs={
+                }
+            ),
+
+            'data_inicio': forms.DateTimeInput(
+                attrs={
+                }
+            )
+        }
