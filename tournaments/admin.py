7c19c4bc3d5ceee92b90dc29b9bab02c7dfc4242
Luiz Gustavo <gustavo120wa@gmail.com>
2026-06-09 19:23:13 -0300

    feat: add models para admin

--- tournaments/admin.py
+++ tournaments/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from tournaments.models import Torneio
+from tournaments.models import Torneio, TorneioParticipante
 
 # Register your models here.
 admin.site.register(Torneio)
+
+admin.site.register(TorneioParticipante)
