8086657b6184eb473f86f3e521b375ced206e531
Italo Felipe <121637958+Iokanaan-dev@users.noreply.github.com>
2026-06-09 15:12:09 -0300

    Concertando urls

--- bracketmaster/urls.py
+++ bracketmaster/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views.index import home
+from .views.index import home, profile_view
 from .views.dashboard import dashboard
 from .views.explore_torneios import explore_torneios
 from .views.explore_profile import explore_profile
@@ -32,8 +32,15 @@ urlpatterns = [
     path('', home, name='home'),
     path('dashboard/', dashboard, name='dashboard'),
     path('users/', include('users.urls')),
+    path('perfil/<str:nickname>/', profile_view, name='profile_view'),
     path('explore_torneios/', explore_torneios , name='explore_torneios'),
     path('explore_perfis/', explore_profile, name='explore_profile'),
     path('torneios_view/', torneios_view, name='torneio_view')
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
